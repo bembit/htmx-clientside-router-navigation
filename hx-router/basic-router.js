92aaa7ec625638e559d3371a031b2f77f856729b
madarben <bencemadar89@gmail.com>
2024-06-13 09:46:51 +0200

    playing with whatever I can with pure clientside..

--- hx-router/basic-router.js
+++ hx-router/basic-router.js
@@ -1,18 +1,19 @@
 (function() {
+    const validRoutes = Array.from(document.querySelectorAll('a[hx-ext="router"]')).map(anchor => anchor.getAttribute('hx-get'));
+    validRoutes.push('/');
+    console.log('validRoutes', validRoutes);
     htmx.defineExtension('router', {
         onEvent: function(name, evt) {
             if (name === "htmx:configRequest") {
-                // the anchor with the hx-ext="router" attribute
                 const target = evt.detail.elt;
                 const url = target.getAttribute('hx-get');
-                // keep track of where to render, return
                 const targetSelector = target.getAttribute('hx-target');
                 const pageTitle = target.getAttribute('data-page-title') || document.title;
 
-                if (url) {
+                if (url && validRoutes.includes(url)) {
                     console.log(target);
                     console.log('url', url);
-                    history.pushState({url: url, target: targetSelector}, "", url);
+                    history.pushState({ url: url, target: targetSelector }, "", "/");
                     document.title = pageTitle;
                 }
             }
@@ -21,11 +22,12 @@
     window.addEventListener('popstate', function(event) {
         if (event.state && event.state.url) {
             const target = event.state.target;
-            htmx.ajax('GET', event.state.url, { target: target });
+            if (validRoutes.includes(event.state.url)) {
+                htmx.ajax('GET', event.state.url, { target: target });
+            } 
         }
-        // fallback to / as /index
         if (window.location.pathname === '/' || window.location.pathname === '' || window.location.pathname === '/index' || window.location.pathname === '/index.html') {
-            history.replaceState({url: '/', target: 'body'}, "", '/');
+            history.replaceState({ url: '/', target: 'body' }, "", '/');
         }
     });
 })();
