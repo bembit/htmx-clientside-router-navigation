c64ceca3d576e8065a0585e2b4f28327b30c4e63
madarben <bencemadar89@gmail.com>
2024-06-16 17:58:26 +0200

    so messy, I love it

--- router-url.js
+++ router-url.js
@@ -1,7 +1,19 @@
 (function() {
-    var extractedRoutes = Array.from(document.querySelectorAll('a[hx-ext="router"]')).map(anchor => anchor.getAttribute('hx-get'));
-    var baseRoutes = ['/'];
-    var validRoutes = [...baseRoutes, ...extractedRoutes];
+    var validRoutes = updateRoutes();
+    function updateRoutes() {
+        var extractedRoutes = Array.from(document.querySelectorAll('a[hx-ext="router"]')).map(anchor => anchor.getAttribute('hx-get'));
+        var baseRoutes = ['/'];
+        var validRoutes = [...baseRoutes, ...extractedRoutes];
+        console.log('updateRoutes function', validRoutes);
+        return validRoutes;
+    }
+    function checkForDuplicates(array) {
+        return array.filter((value, index) => array.indexOf(value) === index);
+    }
+    var validRoutes = checkForDuplicates(validRoutes);
+    console.log('last one after duplicate check', validRoutes);
+    // window.addEventListener('htmx:afterSettle', updateRoutes, console.log('this one from the afterSettle event',validRoutes));
+    window.addEventListener('htmx:afterSettle', updateRoutes, console.log('this one from the afterSettle event',validRoutes));
     htmx.defineExtension('router', {
         onEvent: function(name, evt) {
             if (name === "htmx:configRequest") {
@@ -9,7 +21,8 @@
                 var url = target.getAttribute('hx-get');
                 var targetSelector = target.getAttribute('hx-target');
                 var pageTitle = target.getAttribute('data-page-title') || document.title;
-                if (url && validRoutes.includes(url)) {
+                // would ideally check if route is the same as the current one
+                if (url && (url !== window.location.pathname) && validRoutes.includes(url)) {
                     history.pushState({ url: url, target: targetSelector }, "", url);
                     document.title = pageTitle;
                 }
